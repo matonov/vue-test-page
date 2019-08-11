228cc9a82c76024ba1a107e5a423f8336c804ab7
Martin Novotny <novotny.martin253@gmail.com>
2019-08-11 14:33:56 +0200

    zobrazovanie ceny

--- vue-test-page.js
+++ vue-test-page.js
@@ -1,4 +1,7 @@
 var DATA_URL = 'https://jsonstorage.net/api/items/aa5ea59c-da25-4c76-a8ad-c8f2cd825b27';
+var CURRENCY_TABLE = {
+    'EUR': '€'
+};
 
 var app = new Vue({
     el: '#app',
@@ -12,6 +15,46 @@ var app = new Vue({
           .then(json => {
             this.jsonData = json;
           });
-      }
+    },
+    computed: {
+        price() {
+            return getPrice(this.jsonData.Price, this.jsonData.CurrencyCode);
+        },
+        oldPrice() {
+            return getPrice(this.jsonData.OldPrice, this.jsonData.CurrencyCode);
+        },
+        discount() {
+            return getDiscount(this.jsonData.Discount);
+        }
+    }
 });
 
+/**
+ * Funckcia vrati formatovanu cenu
+ * @param {Number} priceValue hodnota ceny
+ * @param {String} currencyCode kod meny
+ */
+function getPrice(priceValue, currencyCode) {
+    if (priceValue == null && currencyCode == null) {
+        return '';
+    }
+
+    var currency = CURRENCY_TABLE[currencyCode];
+    if (!currency) {
+        currency = currencyCode;
+    }
+
+    return priceValue + currency;
+}
+
+/**
+ * Funkcia vrati formatovanu zlavu
+ * @param {String} discount hodnota zlavy
+ */
+function getDiscount(discount) {
+    if (discount == null) {
+        return '';
+    }
+
+    return discount + ' zľava';
+}
